ba2aa125c7e67495477f2fced7a90729ea45d7f5
wupeng77 <1217558119@qq.com>
2019-09-17 10:32:31 +0800

    练习记录

---
+++ src/main/java/wp/learn/App.java
@@ -0,0 +1,13 @@
+package wp.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/wp/learn/Channel.java
@@ -0,0 +1,41 @@
+/*
+ * 文件名: Channel.java
+ * 文件编号: 
+ * 版权: Copyright (c) 2019, YAN Co.Ltd. and/or its affiliates. All rights reserved.Use is subject to license terms.
+ * 描述: TODO
+ * 创建人: wpeng
+ * 创建时间: 2019-9-17
+ * 修改人:
+ * 修改时间: 2019-9-17
+ * 修改变更号: 
+ * 修改内容: TODO
+ */
+package wp.learn;
+
+/**
+ * @Title Channel
+ * @Description TODO
+ * @author wpeng
+ * @date 2019-9-17
+ * @version V1.0
+ * @see 
+ * @since V1.0
+ * 
+ */
+public class Channel {
+
+    /**
+     * @Title main
+     * @Description 练习通道channel
+     * @author wpeng
+     * @date 2019-9-17
+     * @version V1.0
+     * @param args 
+     * @since V1.0
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
